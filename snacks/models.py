36b671e17a59b4ceb85cafef07d384ffa6ba7560
Ella Svete <ella.svete@gmail.com>
2022-05-24 14:46:49 -0700

    fixed link and added tests

--- snacks/models.py
+++ snacks/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 class Snacks(models.Model):
   name = models.CharField(max_length=64)
-  description = models.TextField()
+  description = models.TextField(default=None)
   purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
   def __str__(self):
--- snacks/urls.py
+++ snacks/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from .views import SnacksDetailView, SnacksListView
 
 urlpatterns = [
   path('', SnacksListView.as_view(), name='snack_list'),
-  path('', SnacksDetailView.as_view(), name='snack_detail')
+  path('<int:pk>/', SnacksDetailView.as_view(), name='snack_detail')
 ]
