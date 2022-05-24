82636f1fbaa54672b8cd8c2c358805b6dd1d04ee
Ella Svete <ella.svete@gmail.com>
2022-05-24 13:49:07 -0700

    list and detail view

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snacks
 
 # Create your views here.
+class SnacksListView(ListView):
+  template_name = 'snack_list.html'
+  model = Snacks
+
+class SnacksDetailView(DetailView):
+  template_name = 'snack_detail.html'
+  model = Snacks
